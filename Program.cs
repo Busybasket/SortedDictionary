9aa4bfc756d391ce278bd460e6e5be35aa6c4ef4
Emin <hakanmehmet849@gmail.com>
2024-12-03 17:01:00 +0300

    initialize project
    1.0

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Program
+{
+    class Program
+    {
+        static SortedDictionary<string, List<string>> sd = new SortedDictionary<string, List<string>>();
+
+        static void Main()
+        {
+            insert();
+            print();
+        }
+
+        static void insert()
+        {
+            string[] lines = File.ReadAllLines(@"C:\\Users\\mhmtm\\source\\repos\\SortedDictionary\\kayitlar.txt");
+            foreach (var line in lines)
+            {
+                string[] parts = line.Split("|");
+                if (parts.Length == 3) 
+                {
+                    string isim = parts[0].Trim();
+                    string şehir = parts[1].Trim();
+                    string telefon = parts[2].Trim();
+
+                    string personInfo = String.Format("{0} - {1}", isim, telefon);
+
+                    if (!sd.ContainsKey(şehir))
+                    {
+                        sd[şehir] = new List<string>();
+                    }
+
+                    sd[şehir].Add(personInfo);
+                }
+            }
+        }
+
+        static void print()
+        {
+            foreach (var city in sd)
+            {
+                Console.WriteLine("{0} :", city.Key);
+                foreach (var person in city.Value)
+                {
+                    Console.WriteLine("  {0}", person);
+                }
+            }
+        }
+    }
+}
